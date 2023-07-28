221d24a89981270fe73e84ee1d8e44422ba1136e
Kamil Kulak <kamil.kulak@talan.com>
2023-07-28 12:00:32 +0200

    Added routings + paths with path param

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WatchComponent } from './watch/watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent
+  },
+  {
+    path: 'watch/:id',
+    component: WatchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-watch',
   templateUrl: './watch.component.html',
   styleUrls: ['./watch.component.css']
 })
-export class WatchComponent {
+export class WatchComponent{
+  // id: string | null
+
+  // constructor(private route: ActivatedRoute) {
+  //   this.id = null
+  // }
+
+  // ngOnInit(): void {
+  //   this.route.params.subscribe(params => {
+  //     this.id = params['id']
+  //   })
+  // }
+
+  constructor(private readonly route: ActivatedRoute) {
+  }
+
+  id: number | null = Number(this.route.snapshot.paramMap.get('id'))
+
+  // 2 podejscie (subscribe wykorzystany w tle)
+  id$ = this.route.params.pipe(switchMap((params) => params['id']))
 
 }
